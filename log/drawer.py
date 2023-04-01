bbeaeae3a10181468ba425b189c9ff7f257bf9d2
zsz <buaazsz@buaa.edu.cn>
2023-04-01 09:29:19 +0800

    finish 2348train

--- log/drawer.py
+++ log/drawer.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.dirname(__file__))
 print(sys.path)
 
 src = []
-fp32 = []
+int8 = []
 int2 = []
 src_int = []
 
@@ -15,14 +15,14 @@ def get_acc(file_name, target_list):
         target_list.append(float(content[2]))
 
 get_acc('result_fp.txt',src)
-get_acc('result_fp32.txt', fp32)
-get_acc('result_int2.txt', int2)
+get_acc('result_int8_bits[2,3,4,8]', int8)
+get_acc('result_int2_bits[2,3,4,8].txt', int2)
 get_acc('result_int.txt', src_int)
 import matplotlib.pyplot as plt
 
 epoch = range(200)
 plt.plot(epoch,src,c='red',label='fp32')
-plt.plot(epoch,fp32,c='pink',label='fp32_quant')
+plt.plot(epoch,int8,c='pink',label='int8_quant')
 plt.plot(epoch,int2,c='cyan',label='int2_quant')
 plt.plot(epoch,src_int,c='blue',label='int2')
 plt.legend(loc='best')
